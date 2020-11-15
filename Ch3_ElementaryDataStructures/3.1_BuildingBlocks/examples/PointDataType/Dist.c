ef173bea72a339cf72e9a051fa0e06da9ce0fcca
Chahat08 <44064355+Chahat08@users.noreply.github.com>
2020-11-15 09:56:10 +0530

    Revert "Add files via upload"

--- Ch3_ElementaryDataStructures/3.1_BuildingBlocks/examples/PointDataType/Dist.c
+++
@@ -1,8 +0,0 @@
-/*This is the implementation for the point data type.*/
-#include "Point.h"
-#include <math.h>
-float distance(point a, point b)
-{
-	float dx = a.x - b.x, dy = a.y - b.y;
-	return sqrt(dx * dx + dy * dy);
-}
--- Ch3_ElementaryDataStructures/3.1_BuildingBlocks/examples/PointDataType/Point.h
+++
@@ -1,3 +0,0 @@
-/*This is the interface for the point data type.*/
-typedef struct { float x; float y; } point;
-float distance(point a, point b);
